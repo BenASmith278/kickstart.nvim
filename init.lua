f6635597d9d4b9291334c4754c2a6f4473dff597
Benjamin Smith <benasmith278@gmail.com>
2026-02-12 12:42:50 -0500

    fix lsps

--- init.lua
+++ init.lua
@@ -603,7 +603,6 @@ require('lazy').setup({
             client.server_capabilities.documentRangeFormattingProvider = false
           end,
         },
-        awk_ls = {},
         -- rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -611,12 +610,11 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
-        --
 
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
-          -- capabilities = {},
+          capabilities = {},
           settings = {
             Lua = {
               completion = {
@@ -640,13 +638,23 @@ require('lazy').setup({
       --    :Mason
       --
       -- You can press `g?` for help in this menu.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'lua_ls', -- Lua Language server
+      local ensure_installed = {
+        'lua-language-server', -- Lua Language server
         'stylua', -- Used to format Lua code
-        'black',
         -- You can add other tools here that you want Mason to install
-      })
+        -- PYTHON --
+        'basedpyright',
+        'black',
+        'debugpy',
+        'flake8',
+        -- JS & TS --
+        'typescript-language-server',
+        'biome',
+        'js-debug-adapter',
+        -- OTHER --
+        'awk-language-server',
+        'vim-language-server',
+      }
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
